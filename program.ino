a9c187981d98013bb7ce516ed376908ffa881934
Bhuvan Pratap Agarwal <60248682+Bhuvan-Pratap-Agarwal@users.noreply.github.com>
2021-06-18 20:27:06 +0530

    Add files via upload

---
+++ program.ino
@@ -0,0 +1,50 @@
+#include <servo.h>
+//IR sensor
+int IRL = 1;
+int IRL = 2;
+int IRL = 3;
+//Ultrasonic sensor
+#define eco1 4;
+#define trig1 5;
+#define eco2 6;
+#define trig2 7;
+//Wheels Motors
+int motorl1 = 8;
+int motorl2 = 9;
+int motorr1 = 10;
+int motorr2 = 11;
+//Servo Motors
+
+//Real Time Clock
+//
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+void linefollower()
+{
+  
+}
+
+//line follower
+//object detection
+void RFID()
+{
+
+}
+
+void Ultrasonic()
+{
+}
+
+void medicines()
+{
+}
+
+}
+//
