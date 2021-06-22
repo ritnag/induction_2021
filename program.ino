a3017ecda4704b8972caed115654602e06ede0e2
Bhuvan Pratap Agarwal <60248682+Bhuvan-Pratap-Agarwal@users.noreply.github.com>
2021-06-22 19:14:51 +0530

    Update program.ino

--- program.ino
+++ program.ino
@@ -4,21 +4,29 @@
 #include <DS1307RTC.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include <ESPS8266WiFi.h>
+//#include <ESPS8266WiFi.h>
 #include <SoftwareSerial.h>
 #include <FirebaseArduino.h>
 #include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
+#include "RTClib.h"
 
-#define FIREBASE_HOST ""
-#define FIREBASE_AUTH""
-#define WIFI_SSID""
-#define WIFI_PASSWORD""
+
+// #define FIREBASE_HOST ""
+// #define FIREBASE_AUTH""
+// #define WIFI_SSID""
+// #define WIFI_PASSWORD""
 String myString;
 char rdata;
 int adata = 0;
 
 int irstVal;
+
+
+
+
+
+
 
 //IR sensor
 //int IRL = A0;
@@ -27,26 +35,29 @@ int IRH = 2;
 int IRO = 3;
 
 //Ultrasonic sensor
-#define eco1 4;
-#define trig1 5;
-#define eco2 6;
-#define trig2 7;
+// #define eco1 4;
+// #define trig1 5;
+// #define eco2 6;
+// #define trig2 7;
 
-//Wheels Motors
+//Line Follower 
 #define in1 9
 #define in2 8
 #define in3 7
 #define in4 6
 #define enA 10
 #define enB 5
-int M1_Speed = 80; // speed of motor 
-int M2_Speed = 80; // speed of motor 2
+int M1_Speed = 100; // speed of motor 
+int M2_Speed = 100; // speed of motor 2
 int LeftRotationSpeed = 250;  // Left Rotation Speed
 int RightRotationSpeed = 250; // Right Rotation Speed
   
 //Servo Motors
 
 //Real Time Clock
+RTC_DS1307 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
 
 //Buzzer
 int buz 12;
@@ -57,6 +68,13 @@ int Gled 14;
 
 //IOT
 SoftwareSerial mySerial(2, 3); // RX, TX
+
+//RFID
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
+
+//=========================================================================   VOID SETUP   ==============================================================================================
 
 void setup() 
 {
@@ -71,9 +89,25 @@ void setup()
   pinMode(A1, INPUT); // initialize Right sensor as an input
         
   //RTC
+  //  while (!Serial); // for Leonardo/Micro/Zero
   Serial.begin(9600);
-  while(!serial);
-  delay(200);
+  if (! rtc.begin())
+  {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (! rtc.isrunning())
+  {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  } 
+
+
   
   //RFID  
   Serial.begin(9600);   
@@ -81,19 +115,23 @@ void setup()
   mfrc522.PCD_Init();  
   Serial.println("Approximate your card to the reader...");       //doubt
   Serial.println();
+
+
   
   //IR Sensor
   pinMode (IRH, INPUT); // sensor pin INPUT
   
   //Ultrasonic Sensor
-  pinMode(trig1, OUTPUT); 
-  pinMode(eco1, INPUT); 
-  pinMode(trig2, OUTPUT); 
-  pinMode(eco2, INPUT); 
-  Serial.begin(9600);
-  Serial.println("Ultrasonic Sensor HC-SR04 Test");             //statement required to be printed?
-  Serial.println("with Arduino UNO R3");
-  
+//  pinMode(trig1, OUTPUT); 
+//  pinMode(eco1, INPUT); 
+//  pinMode(trig2, OUTPUT); 
+//  pinMode(eco2, INPUT); 
+//  Serial.begin(9600);
+//  Serial.println("Ultrasonic Sensor HC-SR04 Test");             //statement required to be printed?
+//  Serial.println("with Arduino UNO R3");
+//  
+
+
   //Servo Motors
   
   //Stepper Motor
@@ -104,6 +142,8 @@ void setup()
   //LED
   digitalWrite(Rled, OUTPUT);
   digitalWrite(Gled, OUTPUT);
+  
+  
   //
   
   pinMode(LED_BUILTIN, OUTPUT);                            // EXAMPLE ONE
@@ -139,10 +179,16 @@ void loop() {
  server.handleClient();
 }
 
+
+//========================================================================  VOID LOOP  =================================================================================================
+
 void loop() 
 {
   linefollower();
 }
+
+
+//=================================================================   LINE FOLLOWER FUNCTION   =================================================================================================
 
 
 void linefollower() //line follower
@@ -164,6 +210,15 @@ void linefollower() //line follower
   else if(RIGHT_SENSOR==1 && LEFT_SENSOR==1) 
   {
    Stop();  //STOP
+   right();
+   delay(5000);
+   stop();
+   despensing(); // despensing or RIFD have to read RFID and choose
+   delay(10000);
+   left();
+   delay(5000);
+   stop();
+   forward();
   }
 }  
  
@@ -211,54 +266,61 @@ void Stop()
   digitalWrite(in4, LOW);
 }
 
+
+
+
+//==============================================================   RFID ANTENNA   ==========================================================================================================
+
+
 void RFID() //Patent Identification
 { 
-  #define SS_PIN 10
-  #define RST_PIN 9
-  MFRC522 mfrc522(SS_PIN, RST_PIN);   
-
-void loop() 
-{
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  void loop() 
   {
-    return;
-  }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
-  {
-    return;
-  }
-  //Show UID on serial monitor
-  Serial.print("UID tag :");
-  String content= "";
-  byte letter;         //??
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));      //doubt
-  }
-  Serial.println();
-  Serial.print("Message : ");
-  content.toUpperCase();
-  if (content.substring(1) == "BD 31 15 2B")  //change here the UID of the card that you want to give access to
-  {
-    Serial.println("Authorized access");
+    // Look for new cards
+    if ( ! mfrc522.PICC_IsNewCardPresent()) 
+    {
+      return;
+    }
+    // Select one of the cards
+    if ( ! mfrc522.PICC_ReadCardSerial()) 
+    {
+      return;
+    }
+    //Show UID on serial monitor
+    Serial.print("UID tag :");
+    String content= "";
+    byte letter;         //??
+    for (byte i = 0; i < mfrc522.uid.size; i++) 
+    {
+       Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+       Serial.print(mfrc522.uid.uidByte[i], HEX);
+       content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+       content.concat(String(mfrc522.uid.uidByte[i], HEX));      //doubt
+    }
     Serial.println();
-    delay(3000);
-  }
- 
- else   {
-    Serial.println(" Access denied");
-    delay(3000);
-  }
-} 
-
+    Serial.print("Message : ");
+    content.toUpperCase();
+    if (content.substring(1) == "BD 31 15 2B")  //change here the UID of the card that you want to give access to
+    {
+      Serial.println("Authorized access");
+      Serial.println();
+      delay(3000);
+    }
+   
+   else
+   {
+      Serial.println(" Access denied");
+      delay(3000);
+    }
+  } 
 }
 
-void medicines() //Despensing system
+
+//==================================================================  DESPENSING   ======================================================================================================
+
+
+
+void despensing() //Despensing system
 {
   Servo motor;
   int p=9;        //digital pin connected
@@ -279,27 +341,92 @@ void medicines() //Despensing system
       delay(10);
     }
 }
-  
+
+void motor1()
+{
+
 }
+
+void motor2()
+{
+
+}
+
+void motor3()
+{
+
+}
+
+void motor4()//main dispeinsing
+{
+
+}
+}
+  
+  
+  
+//====================================================================   IR SENSOR   ====================================================================================================
+
 void irsensor() //despensing gate way
 {
-  int statusSensor = digitalRead (IRSensor);
+  int statusSensor = digitalRead(IRSensor); // reads the IR sensor input
   
-  if (statusSensor == 1)
-    digitalWrite(LED, LOW); // LED LOW
-  }
-  else
-  {
-    digitalWrite(LED, HIGH); // LED High
-  }
+  if (statusSensor == 0)
+     despensing();
 }
-void time()
+
+
+//======================================================================  RTC   ==================================================================================================
+
+  void time()
 {
-  int time; 
-  tmElements_t tm;
-  time = RTC.read(tm);
-  return time;
+  DateTime now = rtc.now();
+
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+
+  Serial.print(" since midnight 1/1/1970 = ");
+  Serial.print(now.unixtime());
+  Serial.print("s = ");
+  Serial.print(now.unixtime() / 86400L);
+  Serial.println("d");
+
+  // calculate a date which is 7 days and 30 seconds into the future
+  DateTime future (now + TimeSpan(7, 12, 30, 6));
+
+  Serial.print(" now + 7d + 30s: ");
+  Serial.print(future.year(), DEC);
+  Serial.print('/');
+  Serial.print(future.month(), DEC);
+  Serial.print('/');
+  Serial.print(future.day(), DEC);
+  Serial.print(' ');
+  Serial.print(future.hour(), DEC);
+  Serial.print(':');
+  Serial.print(future.minute(), DEC);
+  Serial.print(':');
+  Serial.print(future.second(), DEC);
+  Serial.println();
+
+  Serial.println();
+  delay(3000);
 }
+
+
+
+//=======================================================================  BUZZER  =================================================================================================
 
 void buzzReady()
 {
@@ -323,6 +450,10 @@ void buzzDone()
   
   server.handleClient();                    // EXAMPLE  ONE
 }
+
+ 
+
+//==============================================================   SERIAL COMMUNICATION   ==========================================================================================================
 
 void connection()
 {
